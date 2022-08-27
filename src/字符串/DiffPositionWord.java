636d57e75ae2de28e4c727800ba7f1f920eb101c
yao <summerpear@foxmail.com>
2022-08-27 11:19:48 +0800

    reduce loop

--- src/字符串/DiffPositionWord.java
+++ src/字符串/DiffPositionWord.java
@@ -29,8 +29,6 @@ public class DiffPositionWord {
         int[] charCount = new int[26];
         for (int i = 0; i < s.length(); i++) {
             charCount[s.charAt(i) - 'a']++;
-        }
-        for (int i = 0; i < t.length(); i++) {
             charCount[t.charAt(i) - 'a']--;
         }
         for (int i : charCount) {
