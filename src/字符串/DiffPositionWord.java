afa7aeb01535c20cb6d060c622ba23c0608a40d3
yao <summerpear@foxmail.com>
2022-08-27 12:00:46 +0800

    优化

--- src/字符串/DiffPositionWord.java
+++ src/字符串/DiffPositionWord.java
@@ -1,5 +1,7 @@
 package 字符串;
 
+import java.util.Arrays;
+
 /**
  * 给定两个字符串 s 和 t ，编写一个函数来判断 t 是否是 s 的字母异位词
  */
@@ -39,8 +41,45 @@ public class DiffPositionWord {
         return true;
     }
 
+    public boolean isAnagram2(String s, String t) {
+        char[] chars1 = s.toCharArray();
+        char[] chars2 = t.toCharArray();
+        Arrays.sort(chars1);
+        Arrays.sort(chars2);
+        return Arrays.equals(chars1, chars2);
+    }
+
+    /**
+     * only loop one time
+     */
+    public boolean isAnagram3(String s, String t) {
+        if (s.length() != t.length()) {
+            return false;
+        }
+        char[] cArr = s.toCharArray();
+        char[] tArr = t.toCharArray();
+        //26个字母中每个字母出现次数的数组
+        int[] charArr = new int[26];
+        //s字符串中所有
+        int count = 0;
+        //遍历两字符数组
+        for (int i = 0; i < cArr.length; i++) {
+            //若cArr数组中的某一字符在charArr是第一次出现，则计数器加一
+            if (++charArr[cArr[i] - 'a'] == 1) {
+                count++;
+            }
+            //若tArr数组中的某一字符在charArr中已出现，则计数器减一
+            if (--charArr[tArr[i] - 'a'] == 0) {
+                count--;
+            }
+        }
+        return count == 0;
+    }
+
     public static void main(String[] args) {
         System.out.println(new DiffPositionWord().isAnagram("anagram", "nagaram"));
         System.out.println(new DiffPositionWord().isAnagram1("anagram", "nagaram"));
+        System.out.println(new DiffPositionWord().isAnagram2("anagram", "nagaram"));
+        System.out.println(new DiffPositionWord().isAnagram3("abc", "cba"));
     }
 }
