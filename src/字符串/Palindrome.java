c994b7ddc6b0be678a446b351a2c7fa8b14e3c4d
yao <summerpear@foxmail.com>
2022-08-29 23:20:57 +0800

    回文数

---
+++ src/字符串/Palindrome.java
@@ -0,0 +1,71 @@
+package 字符串;
+
+import java.util.Locale;
+
+public class Palindrome {
+
+    public static boolean isPalindrome(String s) {
+        if (s == null) {
+            return false;
+        }
+        s = s.replaceAll("[^A-Za-z0-9]", "").toLowerCase();
+        int left = 0;
+        int right = s.length() - 1;
+        while (left < right) {
+            if (s.charAt(left) != s.charAt(right)) {
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
+
+    public static boolean isPalindrome1(String s) {
+        if (s == null) {
+            return false;
+        }
+        s = s.toLowerCase();
+        int left = 0;
+        int right = s.length() - 1;
+        while (left < right) {
+            while (left < right && !Character.isLetterOrDigit(s.charAt(left))) {
+                left++;
+            }
+            while (left < right && !Character.isLetterOrDigit(s.charAt(right))) {
+                right--;
+            }
+            if (s.charAt(left) != s.charAt(right)) {
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
+
+    public static boolean isPalindrome3(String s) {
+        if (s == null) {
+            return false;
+        }
+        s = s.toLowerCase();
+        for (int i = 0, j = s.length() - 1; i < j; i++, j--) {
+            while (i < j && !Character.isLetterOrDigit(s.charAt(i))) {
+                i++;
+            }
+            while (i < j && !Character.isLetterOrDigit(s.charAt(j))) {
+                j--;
+            }
+            if (s.charAt(i) != s.charAt(j)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(isPalindrome("A man, a plan, a canal: Panama"));
+        System.out.println(isPalindrome1("......a....."));
+        System.out.println(isPalindrome3("A man, a plan, a canal: Panama"));
+    }
+}
