35b4ce0013109a31d444fde28291812d59aca261
yao <summerpear@foxmail.com>
2022-10-22 00:05:40 +0800

    palindrome linked

---
+++ src/链表/Palindrome.java
@@ -0,0 +1,42 @@
+package 链表;
+
+import java.util.Stack;
+
+public class Palindrome {
+    public boolean isPalindrome(ListNode head) {
+        int length = length(head);
+        if (length == 1) {
+            return true;
+        }
+        Stack<ListNode> stack = new Stack<>();
+        ListNode node = head;
+        while (node != null) {
+            stack.push(node);
+            node = node.next;
+        }
+        ListNode next = head;
+        for (int i = 0; i < length; i++) {
+            ListNode pop = stack.pop();
+            if (pop.val != next.val) {
+                return false;
+            }
+            next = next.next;
+        }
+        return true;
+    }
+
+    private int length(ListNode head) {
+        int len = 0;
+        while (head != null) {
+            len++;
+            head = head.next;
+        }
+        return len;
+    }
+
+    public static void main(String[] args) {
+        ListNode node = new ListNode(1, new ListNode(0, new ListNode(1)));
+        boolean palindrome = new Palindrome().isPalindrome(node);
+        System.out.println(palindrome);
+    }
+}
