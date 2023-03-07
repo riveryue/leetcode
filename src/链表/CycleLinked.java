5ea83a2625dbca287cf8a55b4cae3a423aefd463
yao <summerpear@foxmail.com>
2023-03-07 09:54:31 +0800

    fast slow pointer

--- src/链表/CycleLinked.java
+++ src/链表/CycleLinked.java
@@ -16,4 +16,22 @@ public class CycleLinked {
         }
         return false;
     }
+
+    public boolean hasCycleByFastSLow(ListNode head) {
+        if (head == null) {
+            return false;
+        }
+        ListNode fast = head;
+        ListNode slow = head;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+
+            if (fast == slow) {
+                return true;
+            }
+
+        }
+        return false;
+    }
 }
